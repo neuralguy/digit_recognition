47ba392aefc64065936874350d108f747efea19f
neuralguy <109704567+neuralguy@users.noreply.github.com>
2024-01-12 00:11:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,18 @@ import matplotlib.pyplot as plt
 from img_change import change
 
 
-model = keras.models.load_model("model")
+model = keras.models.load_model("models/classnumber/conv_model")
 
-#pred = model.predict(change("your path")) # Uncomment if you want to upload a photo
-#print(np.argmax(pred))
+
+def class_img(img):
+    img = change(img)
+    pred = model.predict(img)
+    print(np.argmax(pred))
+
+    plt.imshow(img[0], cmap=plt.cm.binary)
+    plt.show()
+
+
+if __name__ == '__main__':
+    img = "imgs/numbers/2.png"    # path to your image
+    class_img(img)
